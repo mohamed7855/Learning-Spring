7b69b1695409324ff1e25a74ab6c76b64be603f3
mohamed7855 <martm7726@gmail.com>
2024-04-13 23:17:12 +0200

    Spring IOC with XML Configuration

---
+++ src/main/java/com/learning/spring/GolfCoach.java
@@ -0,0 +1,11 @@
+package com.learning.spring;
+
+public class GolfCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your putting skills for 2 hours today";
+	}
+
+
+}
---
+++ src/main/java/com/learning/spring/MainApp.java
@@ -0,0 +1,19 @@
+package com.learning.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+	public static void main(String[] args) {
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		// retrieve bean from spring container
+		Coach theCoach = context.getBean("myGolfCoach", Coach.class);
+
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+
+		// close the context
+		context.close();
+	}
+}
