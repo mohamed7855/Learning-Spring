2cb440049053e0a1845e5e87831e2e968b3a126b
mohamed7855 <martm7726@gmail.com>
2024-04-17 16:52:15 +0200

    Using Annotation @Component with XML Configuration

--- src/main/java/com/learning/spring/MainApp.java
+++ src/main/java/com/learning/spring/MainApp.java
@@ -8,26 +8,10 @@ public class MainApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("myGolfCoach", Coach.class);
-		Coach alphaCoach = context.getBean("myGolfCoach", Coach.class);
-		
-		System.out.println(theCoach==alphaCoach);//false
-		System.out.println("Memory location for theCoach: "+theCoach);	  //com.learning.spring.GolfCoach@101952da
-		System.out.println("Memory location for alphaCoach: "+alphaCoach);//com.learning.spring.GolfCoach@6babf3bf
-		
-		Coach theCoach2 = context.getBean("myBaseballCoach", Coach.class);
-		BaseballCoach theCoach3 = context.getBean("myBaseballCoach", BaseballCoach.class);
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
-		
-		System.out.println(theCoach.getDailyFortune());
-		
-		System.out.println(theCoach2.getDailyWorkout());
-		System.out.println(theCoach2.getDailyFortune());
-		
-		System.out.println(theCoach3.getTeam());
-		System.out.println(theCoach3.getEmail());
 
 		// close the context
 		context.close();
