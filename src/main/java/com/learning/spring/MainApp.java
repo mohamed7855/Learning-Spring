0ab519fe9b2c77ac456e3f96d602a88146e4f673
mohamed7855 <martm7726@gmail.com>
2024-04-19 19:56:21 +0200

    Injecting Values from Properties File with Java Code

--- src/main/java/com/learning/spring/MainApp.java
+++ src/main/java/com/learning/spring/MainApp.java
@@ -8,11 +8,13 @@ public class MainApp {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("swimCoach", Coach.class);
+		SwimCoach theCoach = context.getBean("swimCoach", SwimCoach.class);
 		
 		// call methods on the bean
 		System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach.getDailyFortune());
+		System.out.println(theCoach.getEmail());
+		System.out.println(theCoach.getTeam());
 
 		// close the context
 		context.close();
