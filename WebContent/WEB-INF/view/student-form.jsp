bcea94cf834d19d7480fdb2540f73953aeb56590
mohamed7855 <martm7726@gmail.com>
2024-05-18 00:14:31 +0300

    Spring MVC Validation Number

--- WebContent/WEB-INF/view/student-form.jsp
+++ WebContent/WEB-INF/view/student-form.jsp
@@ -20,7 +20,14 @@
 		<form:input id="lName" name="lName" path="lastName" />
 		<form:errors path="lastName" cssClass="error"></form:errors>
 		<br><br>
-		
+		<label for="age">Age (*)</label>
+		<form:input id="age" name="age" path="age" />
+		<form:errors path="age" cssClass="error"></form:errors>
+		<br><br>
+		<label for="code">postalCode (*)</label>
+		<form:input id="code" name="age" path="postalCode" />
+		<form:errors path="postalCode" cssClass="error"></form:errors>
+		<br><br>
 		
 		
 		<button type="submit">Submit</button>
--- src/com/learning/spring/mvc/Student.java
+++ src/com/learning/spring/mvc/Student.java
@@ -1,6 +1,9 @@
 package com.learning.spring.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Student {
@@ -13,6 +16,12 @@ public class Student {
 	@Size(min = 2, message="is required")
 	private String lastName;
 	
+	@Min(value =18 , message = "must be greater than or equal to 18")
+	@Max(value =40 , message = "must be less than or equal to 40")
+	private int age;
+	
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+	private String postalCode;
 	
 	public Student() {
 		
@@ -33,5 +42,23 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	
+	
 
 }
